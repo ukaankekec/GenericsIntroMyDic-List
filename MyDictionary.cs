1288ab964ce7c36e5fcd0f7455601f5bc2a3eb72
ukaan <ukaan@DESKTOP-P2PJBPR>
2021-01-22 21:47:09 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+
+    public class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            // contsructor function starts when the class calls
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+
+            // a control that the new key is in existing keys or not
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].Equals(key))
+                {
+                    throw new ArgumentException("can not be used the same key on an another key!!");
+                }
+            }
+
+            // we're creating new temporary arrays, because when we expand length of the arrays we miss the values that the arrays have
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            // we're giving the values to arrays back
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+
+            }
+            // arrays get the new values
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public void WriteLine()
+        {
+
+            // Foreach loop is not working in this situation
+            int i = 0;
+            while (i<keys.Length)
+            {
+                Console.WriteLine($"[{keys[i]},{values[i]}]");
+                i++;
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyList<string> isimler = new MyList<string>();
+            //isimler.Add("engin");
+            //isimler.Add("kerem");
+
+
+            //Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            //dictionary.Add(1, "kerem");
+            //dictionary.Add(2, "kerem");
+            //dictionary.Add(3, "kerem");
+
+            //foreach (var item in dictionary)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //MyDictionary<int, int> myDictionary = new MyDictionary<int, int>();
+
+            //myDictionary.Add(1, 2);
+            //myDictionary.Add(3, 4);
+
+            //Console.WriteLine(myDictionary);
+
+            MyDictionary<int, int> myDictionary = new MyDictionary<int, int>();
+            
+            myDictionary.Add(1, 2);
+            myDictionary.Add(3, 4);
+
+            myDictionary.WriteLine();
+           
+
+        }
+    }
+}
